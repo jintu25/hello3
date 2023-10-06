8544c8a52bbcc9086183ac23c559a337248f0ee1
Zintu paul <jintupaul45@gmail.com>
2023-10-06 19:42:26 +0600

    hello

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+function Show() {
+    const name = document.getElementById('name').value;
+    if(document.getElementById('name').value=""){
+        alert('please type your name')
+    }
+    else{
+        document.getElementById('addname').innerHTML=name;
+    }
+
+    document.getElementById('m1').style.display='none'
+
+    document.getElementById('c1').style.display='block'
+
+}
+  
+
+
+  
+  
+  
+  
